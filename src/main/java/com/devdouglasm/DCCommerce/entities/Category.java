4c753c5f15d78e965be5bfdad0078918eb63d7eb
devdouglasm <devdouglasm@gmail.com>
2025-03-14 17:13:29 -0300

    creating category and product classes, and use many-to-many

---
+++ src/main/java/com/devdouglasm/DCCommerce/entities/Category.java
@@ -0,0 +1,41 @@
+package com.devdouglasm.DCCommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories") // mapping using the same name of the attribute in Product class
+    private Set<Product> products = new HashSet<>(); // using a set than list, because product can't repeat
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
