978214eb7477ed28a69938575671826caac2ca6a
devdouglasm <devdouglasm@gmail.com>
2025-03-14 12:40:43 -0300

    first commit

---
+++ src/main/java/com/devdouglasm/DCCommerce/DcCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.DCCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdouglasm/DCCommerce/DcCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.DCCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
