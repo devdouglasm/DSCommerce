454eedd613ad92c17067c246b4e8ebfc2b7ea2e6
devdouglasm <devdouglasm@gmail.com>
2025-03-15 11:25:57 -0300

    editing payment class and adding equals and hashcode

--- src/main/java/com/devdouglasm/DCCommerce/entities/Category.java
+++ src/main/java/com/devdouglasm/DCCommerce/entities/Category.java
@@ -3,6 +3,7 @@ package com.devdouglasm.DCCommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -37,5 +38,17 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
--- src/main/java/com/devdouglasm/DCCommerce/entities/Payment.java
+++ src/main/java/com/devdouglasm/DCCommerce/entities/Payment.java
@@ -3,6 +3,7 @@ package com.devdouglasm.DCCommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_payment")
@@ -19,5 +20,46 @@ public class Payment {
     @MapsId
     private Order order;
 
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
