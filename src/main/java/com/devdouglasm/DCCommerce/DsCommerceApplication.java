9ab930603edf0039a59dd84bd7b73ae22ac6d258
devdouglasm <devdouglasm@gmail.com>
2025-09-16 17:26:53 -0300

    Renaming app, adding security dependencies, creating Role

--- src/main/java/com/devdouglasm/DCCommerce/DsCommerceApplication.java
+++ src/main/java/com/devdouglasm/DCCommerce/DsCommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DcCommerceApplication {
+public class DsCommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DcCommerceApplication.class, args);
+		SpringApplication.run(DsCommerceApplication.class, args);
 	}
 
 }
