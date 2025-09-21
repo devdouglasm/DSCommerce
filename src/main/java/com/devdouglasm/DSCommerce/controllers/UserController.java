5f88356b04cf0da7caa237a98ec16a1119cbd3f8
devdouglasm <devdouglasm@gmail.com>
2025-09-21 06:53:38 -0300

    Get user logged

---
+++ src/main/java/com/devdouglasm/DSCommerce/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.devdouglasm.DSCommerce.controllers;
+
+import com.devdouglasm.DSCommerce.dto.UserDTO;
+import com.devdouglasm.DSCommerce.entities.User;
+import com.devdouglasm.DSCommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
--- src/main/java/com/devdouglasm/DSCommerce/services/UserService.java
+++ src/main/java/com/devdouglasm/DSCommerce/services/UserService.java
@@ -1,17 +1,23 @@
 package com.devdouglasm.DSCommerce.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.devdouglasm.DSCommerce.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import com.devdouglasm.DSCommerce.entities.Role;
 import com.devdouglasm.DSCommerce.entities.User;
 import com.devdouglasm.DSCommerce.projections.UserDetailsProjection;
 import com.devdouglasm.DSCommerce.repositories.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -35,5 +41,23 @@ public class UserService implements UserDetailsService {
 		}
 		
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+			return repository.findByEmail(username).get();
+		}
+		catch (Exception e) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User user = authenticated();
+		return new UserDTO(user);
 	}
 }
