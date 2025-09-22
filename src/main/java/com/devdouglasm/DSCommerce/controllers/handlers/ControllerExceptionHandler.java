7e8fdb3b5d514ec624f1eacdecaf149b34bd1211
devdouglasm <devdouglasm@gmail.com>
2025-09-22 04:54:48 -0300

    Validating user or admin

--- src/main/java/com/devdouglasm/DSCommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devdouglasm/DSCommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.devdouglasm.DSCommerce.controllers.handlers;
 import com.devdouglasm.DSCommerce.dto.CustomError;
 import com.devdouglasm.DSCommerce.dto.ValidationError;
 import com.devdouglasm.DSCommerce.services.exception.DatabaseException;
+import com.devdouglasm.DSCommerce.services.exception.ForbiddenException;
 import com.devdouglasm.DSCommerce.services.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -38,6 +39,13 @@ public class ControllerExceptionHandler {
         for (FieldError f : e.getBindingResult().getFieldErrors()) {
             error.addErrors(f.getField(), f.getDefaultMessage());
         }
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<CustomError> forbidden(ForbiddenException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
 }
---
+++ src/main/java/com/devdouglasm/DSCommerce/services/AuthService.java
@@ -0,0 +1,21 @@
+package com.devdouglasm.DSCommerce.services;
+
+import com.devdouglasm.DSCommerce.entities.User;
+import com.devdouglasm.DSCommerce.services.exception.ForbiddenException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserService userService;
+
+    public void validateSelfOrAdmin(long userId) {
+        User me = userService.authenticated();
+        if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
+            throw new ForbiddenException("Access denied");
+        }
+    }
+
+}
